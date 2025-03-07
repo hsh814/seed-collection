5f0370dca155c3ba37c2e055b2b2b25a6077867f
FreddyYJ <kyj1411@unist.ac.kr>
2025-03-07 17:08:17 +0900

    Update distance.py

--- distance.py
+++ distance.py
@@ -1,55 +1,51 @@
-from typing import Dict, List
-
-import numpy as np
-import scipy
 import os
-import sys
-import argparse
+import subprocess
+from typing import Dict
 
-new_seeds:Dict[str,List[np.ndarray]]=dict()
+import sbsv
+import json
 
-arg_parser=argparse.ArgumentParser(prog='distance.py')
-arg_parser.add_argument('dir_1',action='store',type=str,help='Base seed directory')
-arg_parser.add_argument('dir_2',action='store',type=str,help='Target seed directory to compare')
-arg_parser.add_argument('subject',action='store',type=str,help='Target subject')
-args=arg_parser.parse_args(sys.argv[1:])
+subject_data:Dict[str,dict]=dict() # subject: {version: data_dict}
+for path, dirnames, filenames in os.walk('new-seeds-log'):
+    for file in filenames:
+        if file.endswith('.gz'):
+            res=subprocess.run(f'gunzip {file}',shell=True,cwd=path)
+            if res.returncode!=0:
+                raise RuntimeError(f'gunzip failed for {file}')
 
-subject=args.subject
-dir_1=os.path.abspath(args.dir_1)
-dir_2=os.path.abspath(args.dir_2)
+        subject=path.split('/')[-1]
+        version=file.split('.')[0]
+        if subject not in subject_data:
+            subject_data[subject]=dict()
+        print(f'{subject}-{version}')
 
-for path, directories, files in os.walk(f'{dir_2}/{subject}'):
-    for file in files:
-        rel_path=path.removeprefix(f'{dir_2}/{subject}/')
-        if len(rel_path) <= 1:
-            # A root path
-            continue
+        parser=sbsv.parser()
+        parser.add_schema("[seed] [file: str] [hash: int] [dfg: int] [res: int] [time: int] [target: int] [vec: str] [trace: str]")
+        with open(os.path.join(path,file)) as f:
+            parser.load(f)
 
-        if rel_path not in new_seeds:
-            new_seeds[rel_path]=[]
-        with open(os.path.join(path,file),'rb') as f:
-            data=f.read()
-            new_seeds[rel_path].append(np.frombuffer(data, dtype=np.int8))
+        data_dict:Dict[str,dict]=dict()  # patch_file: {target_reached: , dfg: list, branch_cov: list}
+        for val in parser.get_result_in_order(["[seed]"]):
+            target_reached=val['target']==1
+            dfg=[]
+            for v in val['vec'].split(','):
+                if v!='':
+                    dfg.append(int(v))
 
-for path, dirs, files in os.walk(f'{dir_1}/{subject}'):
-    for file in files:
-        rel_path = path.removeprefix(f'{dir_1}/{subject}/')
-        if len(rel_path) <= 1:
-            # A root path
-            continue
+            branch_cov_str:str=val['trace']
+            branch_cov=[]
+            _i=0
+            while _i!=-1:
+                _i=branch_cov_str.find('1',_i)
+                branch_cov.append(_i)
+                _i+=1
+            data_dict[val['file']]={
+                'target_reached':target_reached,
+                'dfg':dfg,
+                'branch_cov':branch_cov
+            }
 
-        with open(os.path.join(path, file), 'rb') as f:
-            data=f.read()
+        subject_data[subject][version]=data_dict
 
-        data_array=np.frombuffer(data,dtype=np.int8)
-        dists=[]
-        for base in new_seeds:
-            for base_data in new_seeds[base]:
-                if len(data_array) > len(base_data):
-                    base_data=np.pad(base_data,(0,len(data_array)-len(base_data)),constant_values=(0,0))
-                else:
-                    data_array=np.pad(data_array,(0,len(base_data)-len(data_array)),constant_values=(0,0))
-                dist=scipy.spatial.distance.hamming(data_array,base_data)
-                dists.append(dist)
-
-        print(f'{os.path.join(path,file)}: mean: {np.mean(dists)}, median: {np.median(dists)}, len: {len(dists)}')
+with open('data.json','w') as f:
+    json.dump(subject_data,f)
